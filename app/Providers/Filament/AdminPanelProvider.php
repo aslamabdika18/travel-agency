2442b0f3387cf4e057b32d6aaa0c2fa6e8546220
Aslam Abdika <aslamabdika@gmail.com>
2025-07-18 17:21:30 +0700

    fix logout redirect

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->spa()
             ->loginRouteSlug('auth')
+            ->logoutRedirectUrl('/')
             ->colors([
                 'primary' => Color::Orange,
             ])
